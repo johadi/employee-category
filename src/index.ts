c267bcfa1d4f8c6b8142490b929108f416efdc48
johadi10 <jim.hadi10@gmail.com>
2018-03-08 00:10:39 +0100

    Add polymorphism demonstration

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@
 
 import { Employee, HourlyRatedEmployee, CommissionBasedEmployee } from './classes';
 
-const employee = new Employee('sff00989', 'Jimoh Hadi', 2000);
-const hourlyRatedEmployee = new HourlyRatedEmployee('sff4536377', 'Oluwafemi Akande', 0, 8, 0.6);
-const commissionBasedEmployee = new CommissionBasedEmployee('sff9836737', 'Taye Ibori', 3000, 400, 0.2);
+const employee: Employee = new Employee('sff00989', 'Jimoh Hadi', 2000);
+const hourlyRatedEmployee: HourlyRatedEmployee = new HourlyRatedEmployee('sff4536377', 'Oluwafemi Akande', 0, 8, 0.6);
+const commissionBasedEmployee: CommissionBasedEmployee = new CommissionBasedEmployee('sff9836737', 'Taye Ibori', 3000, 400, 0.2);
+
+//Polymorphic form
+const newHourlyRatedEmployee: Employee = new HourlyRatedEmployee('sff4536377', 'Olu Ade', 0, 20, 0.8);
 
 console.log(employee.getEmployeeInfo());
 console.log(hourlyRatedEmployee.getEmployeeInfo());
 console.log(commissionBasedEmployee.getEmployeeInfo());
+console.log(newHourlyRatedEmployee.getEmployeeInfo());
